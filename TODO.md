5f9aec5e174b1fa57f3e6719253a04f3be7f32ab
brianhouse <brian.house@gmail.com>
2013-03-14 10:55:12 -0400

    added text view

--- TODO.md
+++ TODO.md
@@ -1,16 +1,11 @@
 
 comment up HTML
 
-text overlay intro, with instructions where to start (no, it would keep coming up)
-
 test with android
-
-text / video -- allow both/either
-
-maybe text should be a separate screen
 
 json validator: http://jsonlint.com/
 
+example template
 
 hosting
 
--- locativor.js
+++ locativor.js
@@ -3,11 +3,13 @@ var mapbox_map_id = "124z30te";
 var path_to_data = "markers.json";
 var show_latlng = true;
 
-var map;
-var current_location_marker;
-var hotspots = [];
-var scale;
+// global variables
+var map;                     // the map object
+var current_location_marker; // dot for the location of the user
+var hotspots = [];           // hotspot data
+var narratives = [];         // holder for text data
 
+/* create the map */
 function initMap () {
     map = new L.map('map', {
         layers: new L.TileLayer("http://a.tiles.mapbox.com/v3/" + mapbox_username + ".map-" + mapbox_map_id + "/{z}/{x}/{y}.png"),
@@ -24,79 +26,76 @@ function initMap () {
         minZoom: 17,                    
         maxZoom: 17
     });     
-    // map.panTo([42.352455, -71.048069]);
-    // map.setZoom(17);    
     map.locate({setView: true, watch: true});
     map.on('locationfound', onLocationFound);
     map.on('locationerror', onLocationError);
 }
 
+/* load data file */
 function loadMarkers () {
     $.getJSON(path_to_data, function(data) {
         var items = [];
         $.each(data, function(index, o) {
-            createHotspot(o['latlng'], o['radius'], o['color'], o['content'], o['video']);
+            createHotspot(o['latlng'], o['radius'], o['color'], o['text'], o['video']);
         });
     });    
 }
 
-function feetToPixels (feet) {
-    return feet * 0.29; // leaflet scale and geodistance disagree
-}
-
-function createHotspot (latlng, radius, color, content, video) {
+/* create hotspots */
+function createHotspot (latlng, radius, color, text, video) {
     var marker = L.circleMarker(latlng, {radius: feetToPixels(radius), clickable: true, color: color}).addTo(map);     
-    if (video != undefined && video.length) {
-        content = '<iframe style="width: 160; height: 90px;" title="YouTube video player" src="http://www.youtube.com/embed/uYMOG2lquSE?HD=1;rel=0;showinfo=0;controls=0"></iframe>';
-        content = '<iframe style="width: 160; height: 90px;" src="http://player.vimeo.com/video/61742609?title=0&amp;byline=0&amp;portrait=0&amp;color=ffff00" webkitAllowFullScreen mozallowfullscreen allowFullScreen ></iframe>';
+    content = '<div style="width: 160px">';
+    if (text != undefined && text.length) {
+        narratives.push(text);
+        content += '<a href="javascript:openNarrative(' + (narratives.length - 1) + ');"><img style="width: 160px; height: 90px;" src="text_icon.png" /></a>';
     }
+    if (video != undefined && video.length) {
+        content += '<iframe style="width: 160; height: 90px;" src="http://player.vimeo.com/video/' + video + '?title=0&amp;byline=0&amp;portrait=0&amp;color=ffff00" webkitAllowFullScreen mozallowfullscreen allowFullScreen ></iframe>';
+    }
+    content += '</div>';
     marker.bindPopup(content, {closeButton: false, autoPan: true});
-    hotspots.push([marker, radius, video]);
+    hotspots.push([marker, radius, text, video]);
 }
 
-function listProperties (obj) {
-   var propList = "";
-   for(var propName in obj) {
-      if(typeof(obj[propName]) != "undefined") {
-         propList += (propName + ", ");
-      }
-   }
-   console.log(propList);
-}
-
-function roundRead (value) {
-    return Math.round(value * 10000) / 10000;
-}
-
+/* when the user's location is found */
 function onLocationFound (e) {
     if (show_latlng) {
+        // show current lat/lng
         $('#current').html(roundRead(e.latlng.lat) + ", " + roundRead(e.latlng.lng));
     }
     if (current_location_marker == null) {
+        // create the user's marker
         current_location_marker = L.circleMarker(e.latlng, {radius: 10, color: "#009dff", stroke: false, fillOpacity: 1.0, clickable: false}).addTo(map);
     } else {
+        // move the user's marker
         current_location_marker.setLatLng(e.latlng);
     }       
+    // open nearby hotspots
     $.each(hotspots, function(index, hotspot) {
         var marker = hotspot[0];
         var radius = hotspot[1];
         var distance = geoDistance(e.latlng, marker.getLatLng());
-        var video = hotspot[2];
-        console.log(distance);
-        console.log(video);
         if (distance < radius) {
             marker.openPopup();
-            // if (video != undefined && video.length) {
-            //     window.location = video;
-            // }
         }
     });       
 }
 
+/* if something goes wrong with finding the user's location */
 function onLocationError (e) {
     alert(e.message);
 }
 
+/* open the narrative screen */
+function openNarrative (id) {
+    $('#narrative #content').html(narratives[id]);
+    $('#narrative').show();
+}
+
+
+//// utilities ////
+
+/* calculate the distance between two physical points in ft */
 function geoDistance (latlng1, latlng2) {
     var R = 6371; // km
     var d_lat = (latlng2.lat - latlng1.lat).toRad();
@@ -109,20 +108,25 @@ function geoDistance (latlng1, latlng2) {
     return (R * c) * 3280.84; // convert to ft
 }
 
+function feetToPixels (feet) {
+    return feet * 0.29; // leaflet scale and geodistance disagree
+}
+
+function roundRead (value) {
+    return Math.round(value * 10000) / 10000;
+}
+
 if (typeof(Number.prototype.toRad) === "undefined") {
     Number.prototype.toRad = function() {
         return this * Math.PI / 180;
     }
 }
 
-$(document).ready(function() {                   
 
+/* executes on load */
+$(document).ready(function() {
     loadMarkers();
     initMap();
-
-    // track the users location and do something with it in hotspots
-    // place hotspot, call up media when within range
-    // get it cool on the mobile
-
-    
+    $('#narrative').hide();
 });
+
